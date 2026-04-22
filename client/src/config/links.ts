d8211cb0aa9eed989e83e151da7fdddc20be77b2
YAZ1D <yazidmouayn21@augustana.edu>
2026-04-21 22:44:28 -0500

    Update resume link

--- client/src/config/links.ts
+++ client/src/config/links.ts
@@ -1,6 +1,6 @@
 export const LINKS = {
   github: "https://github.com/YAZ1D",
   linkedin: "https://www.linkedin.com/in/yazid-mouayn",
-  resume: "https://drive.google.com/file/d/1bzbeGnBUefzM_OqQmUPc6jp5GsEvQgr0/view?usp=sharing",
+  resume: "https://drive.google.com/file/d/1lnvUuY-spskajFE5--nTKPE5eaQDBPJs/view?usp=sharing",
   email: "mailto:mouayny@gmail.com",
 };
